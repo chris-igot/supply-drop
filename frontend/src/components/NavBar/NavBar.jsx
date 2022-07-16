6dd6bef362b8465a3a604fecae82eb5081b828dd
Christiani Igot <c.bensi.igot@gmail.com>
2022-07-15 21:01:21 -0400

    removed unused code

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,56 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios";
-import { Link,useNavigate } from 'react-router-dom'
-import Logout from '../Logout/Logout'
-import './NavBar.css'
-import logo from './logo.webp'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
+import Logout from '../Logout/Logout';
+import './NavBar.css';
+import logo from './logo.webp';
 const NavBar = () => {
     const [user, setUser] = useState();
-    const navigate = useNavigate();
 
     useEffect(() => {
-        axios.get(`http://localhost:8000/api/auth`, { withCredentials: true })
-            .then(res => {
-                setUser(res.data)
+        axios
+            .get(`http://localhost:8000/api/auth`, { withCredentials: true })
+            .then((res) => {
+                setUser(res.data);
             })
-            .catch(err => console.log(err))
-    }, [])
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        axios.post("http://localhost:8000/api/user/logout", {}, { withCredentials: true })
-            .then(res => {
-                setUser(null)
-                navigate('/')
-            })
-            .catch(err => console.log(err))
-    }
-
+            .catch((err) => console.log(err));
+    }, []);
 
     return (
-        <div className='nav flex align-center'>
-            <div className='flex align-center'>
-                <h1 className='title-logo'>Supply <span className='gold'>Drop</span></h1>
-                <img src={logo} alt="" srcset="" className='supply-drop-logo'/>
-                
+        <div className="nav flex align-center">
+            <div className="flex align-center">
+                <h1 className="title-logo">
+                    Supply <span className="gold">Drop</span>
+                </h1>
+                <img src={logo} alt="" srcset="" className="supply-drop-logo" />
             </div>
-            <div className='flex align-center'>
-                <Link to="/" className='no-border' >Home</Link>
-                
-                {user && <Link to={`/account/${user._id}`}>Account</Link>}
-                {user ? 
-                <div className="logged flex align-center">
-                    <Logout />
-                    <p className='user'>
-                        Hello, <span className='username'>{user.firstName} {user.lastName}</span>
-                    </p>
-                </div>
-                : <Link to="/login">Login</Link>}
+            <div className="flex align-center">
+                <Link to="/" className="no-border">
+                    Home
+                </Link>
 
+                {user && <Link to={`/account/${user._id}`}>Account</Link>}
+                {user ? (
+                    <div className="logged flex align-center">
+                        <Logout />
+                        <p className="user">
+                            Hello,{' '}
+                            <span className="username">
+                                {user.firstName} {user.lastName}
+                            </span>
+                        </p>
+                    </div>
+                ) : (
+                    <Link to="/login">Login</Link>
+                )}
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default NavBar
+export default NavBar;
