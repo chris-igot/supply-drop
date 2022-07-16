e270d373de10bb84862467f850ea152ebe81aa64
Christiani Igot <c.bensi.igot@gmail.com>
2022-07-16 01:32:43 -0400

    added mui container

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,31 +9,40 @@ import Logout from './components/Logout/Logout';
 import EditPost from './components/UpdatePosts/UpdatePosts';
 import UserDetail from './views/UserAccount/UserAccount';
 import EditUser from './views/EditUser/EditUser';
+import { Container } from '@mui/material';
 
 function App() {
     return (
         <div className="App">
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/login" element={<Login />}></Route>
-                    <Route path="/register" element={<Register />}></Route>
-                    <Route path="/" element={<Home />}></Route>
-                    <Route path="/create" element={<Home />}></Route>
-                    <Route path="/allposts" element={<AllPost />}></Route>
-                    <Route path="/logout" element={<Logout />}></Route>
+            <Container maxWidth="md">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/login" element={<Login />}></Route>
+                        <Route path="/register" element={<Register />}></Route>
+                        <Route path="/" element={<Home />}></Route>
+                        <Route path="/create" element={<Home />}></Route>
+                        <Route path="/allposts" element={<AllPost />}></Route>
+                        <Route path="/logout" element={<Logout />}></Route>
 
-                    {/* IF YOU ARE DOING ACCOUNT PLZ CHANGE -> will be /account/id try to make id into the username if you can or not */}
-                    <Route path="/account/:id" element={<UserDetail />}></Route>
-                    <Route
-                        path="/account/edit/:id"
-                        element={<EditUser />}
-                    ></Route>
+                        {/* IF YOU ARE DOING ACCOUNT PLZ CHANGE -> will be /account/id try to make id into the username if you can or not */}
+                        <Route
+                            path="/account/:id"
+                            element={<UserDetail />}
+                        ></Route>
+                        <Route
+                            path="/account/edit/:id"
+                            element={<EditUser />}
+                        ></Route>
 
-                    {/* IF YOU ARE DOING VIEWS PLZ CHANGE -> will be /view/id or the name of the post or not*/}
-                    <Route path="/view" element={<View />}></Route>
-                    <Route path="/edit/post/:id" element={<EditPost />}></Route>
-                </Routes>
-            </BrowserRouter>
+                        {/* IF YOU ARE DOING VIEWS PLZ CHANGE -> will be /view/id or the name of the post or not*/}
+                        <Route path="/view" element={<View />}></Route>
+                        <Route
+                            path="/edit/post/:id"
+                            element={<EditPost />}
+                        ></Route>
+                    </Routes>
+                </BrowserRouter>
+            </Container>
         </div>
     );
 }
